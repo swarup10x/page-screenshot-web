f20aae60bca959b31ce1c6a9b04de3aeaea97c18
Swarup Ahmed <swarup692@gmail.com>
2024-05-20 15:16:24 +0600

    first-commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,77 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect } from "react";
 import styles from "./page.module.css";
+import html2canvas from "html2canvas";
 
 export default function Home() {
+
+  var backgroundColors=["#B71C1C","#D81B60","#9C27B0","#673AB7","#1A237E","#33691E","#4b0082","#6a5acd","#ffe5b4","#fffff0","#00a86b"]
+  var textColors=["black","white","blue","gray","gold"]
+
+  useEffect(() => {
+    const changeBackgroundColor = () => {
+      const colorFrame = document.getElementById('color-frame');
+      if (colorFrame) {
+        const randomBackgroundColor = backgroundColors[Math.floor(Math.random() * backgroundColors.length)];
+        colorFrame.style.backgroundColor = randomBackgroundColor;
+      }
+    };
+
+    const changeVercelTextColors = () => {
+      const frameTextVercel = document.getElementById('frame-text-vercel');
+
+      if (frameTextVercel) {
+        const randomTextColor = textColors[Math.floor(Math.random() * textColors.length)];
+        frameTextVercel.style.color = randomTextColor;
+  
+      }
+    };
+    const changeNextTextColors = () => {
+      const frameTextNext = document.getElementById('frame-text-next');
+      if ( frameTextNext) {
+        const randomTextColor = textColors[Math.floor(Math.random() * textColors.length)];
+        frameTextNext.style.color = randomTextColor;
+      }
+    };
+    const backgroundColorInterval = setInterval(changeBackgroundColor, 1500);
+    const textVercelColorInterval = setInterval(changeVercelTextColors, 2000);
+    const textNextColorInterval = setInterval(changeNextTextColors, 2000);
+
+    return () => {
+      clearInterval(backgroundColorInterval);
+      clearInterval(textVercelColorInterval);
+      clearInterval(textNextColorInterval);
+    };
+  }, []);
+
+  const takeScreenshotOfElement = async () => {
+    const element = document.getElementById('color-frame');
+    if (element) {
+      const canvas = await html2canvas(element);
+      const dataUrl = canvas.toDataURL('image/png');
+      const link = document.createElement('a');
+      link.href = dataUrl;
+      link.download = 'color-frame-screenshot.png';
+      link.click();
+    }
+  };
+
+  const takeScreenshotOfFullPage = async () => {
+    const canvas = await html2canvas(document.body);
+    const dataUrl = canvas.toDataURL('image/png');
+    const link = document.createElement('a');
+    link.href = dataUrl;
+    link.download = 'full-page-screenshot.png';
+    link.click();
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          Get started by changing color&nbsp;
+          <code className={styles.code}>Take screenshot of the Frame</code>
         </p>
         <div>
           <a
@@ -15,81 +79,45 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
+            by Swarup
+            
           </a>
         </div>
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        <div className={styles.color_frame} id="color-frame">
+
+        <h1 id="frame-text-vercel">Vercel</h1>
+        <h1 id="frame-text-next">Next/React</h1>
+        </div>
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+      <div className={styles.grid_center}>
+        <button
+          
           className={styles.card}
-          target="_blank"
+          onClick={takeScreenshotOfElement}
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            Screenshot <span>only frame</span>
           </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          
+        </button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        <button
           className={styles.card}
-          target="_blank"
+          onClick={takeScreenshotOfFullPage}
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+          Screenshot <span>full page</span>
+          </h2>          
+        </button>
+        
       </div>
+      
     </main>
   );
 }
